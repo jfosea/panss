f480866ac42d05fcceda575b23f7f159fc864447
josea <janaosea.25@gmail.com>
2021-02-22 13:48:30 -0700

    add train me tab

--- app/app.R
+++ app/app.R
@@ -7,18 +7,21 @@ header <- dashboardHeader(title="Actions")
 
 sidebar <- dashboardSidebar(
   sidebarMenu(
-    menuItem("Welcome Page",tabName = "welcome")
+    menuItem("Welcome",tabName = "welcome"),
+    menuItem("Train Me", tabName = "trainMe")
   )
 )
 
 body <- dashboardBody(
   tabItems(
-    tabItem(tabName = "welcome", welcomePageUI("welcomePage-module"))
+    tabItem(tabName = "welcome", welcomePageUI("welcomePage-module")),
+    tabItem(tabName = "trainMe", trainMePageUI("trainMePage-module"))
   )
 )
 
 server <- function(input, output, session) {
   callModule(welcomePage, "welcomePage-module")
+  callModule(trainMePage, "trainMePage-module")
 }
 
 ui <- dashboardPage(header, sidebar, body)
---
+++ app/modules/trainMePage.R
@@ -0,0 +1,7 @@
+trainMePageUI <- function(id) {
+  ns <- NS(id)
+  tagList(h2("Training Questions"))
+}
+
+trainMePage <- function(input, output, session) {
+}
